626dcee50ddcca72ff8e10f6af7b8112fd908ea8
Sonsawan Ngamsom <86834525+murphy6867@users.noreply.github.com>
2021-10-21 17:47:08 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,1 @@
+def hangman(word): wrong = 0 stages = ["", "________ ", "| ", "| | ", "| 0 ", "| /|\ ", "| / \ ", "| " ] rletters = list(word) board = ["__"] * len(word) win = False print("Welcome to Hangman") while wrong < len(stages) - 1: print("\n") msg = "Guess a letter" char = input(msg) if char in rletters: cind = rletters \ .index(char) board[cind] = char rletters[cind] = ' 
